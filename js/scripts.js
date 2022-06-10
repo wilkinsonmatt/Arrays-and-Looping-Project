fcfa047bb5d3030b3cfb486208a20e3bf00daefd
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-10 15:10:59 -0700

    Made neighborhood function. It will take the zero to inputted number array and replace the elements based on heiracy

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,9 @@ function arrayZeroToInput(inputtedNumber) {
 
 function ElementIncludesNumber(currentElement, number) {
   const numberArray = currentElement.split("");
-  console.log(numberArray);
+  //console.log(numberArray);
   for (const Element of numberArray) {
-    console.log(Element);
+    //console.log(Element);
     if (Element === number) {
       return true
     }
@@ -25,7 +25,29 @@ function ElementIncludesNumber(currentElement, number) {
   return false
 }
 
-let test = ElementIncludesNumber("1jkl   23", "3");
+
+function Neighborhood(zeroToInputArray) {
+  const finalArray = zeroToInputArray.map(function(Element) {
+    if (ElementIncludesNumber(Element, "3")) {
+      console.log(Element);
+      return "Won't you be my neighbor?"
+    } else if (ElementIncludesNumber(Element, "2")) {
+      return "Boop!";
+    } else if (ElementIncludesNumber(Element, "1")) {
+      return "Beep!";
+    } else {
+      return Element;
+    }
+  });
+  return finalArray;
+}
+
+
+
+
+
+
+let test = Neighborhood(["1jkl", "23", "3", "4"]);
 
 console.log(test);
 
